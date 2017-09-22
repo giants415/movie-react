3b296cb5edaf2f4fd49f0d878a729d4a98cbc1eb
Andrew <andrew.vinocur@gmail.com>
2017-09-22 15:00:05 -0700

    changes preset from es2015 to env, consistent w/ babel packages in use

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
                 include: path.resolve(__dirname, 'src'),
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react', 'env']
                 }
             },
             {

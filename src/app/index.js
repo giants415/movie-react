b2b9c704cf81bc5a5f6ac1d3ccf79862e0e55c10
Andrew <andrew.vinocur@gmail.com>
2017-09-26 13:03:08 -0700

    builds basic, currently non-functioning component

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+class UIComponent extends React.component {
+  render() {
+    return(
+      <div>
+        <h1>Hi Vinocur</h1>
+        <h1>Look at me</h1>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<UIComponent />, document.getElementById('movieWrapper'));

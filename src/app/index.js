2dd9e8048abbded489b0aa36f4b23511cdd56aaf
Andrew <andrew.vinocur@gmail.com>
2017-09-26 17:29:14 -0700

    requires and implements search component

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var SearchComponent = require('./SearchComponent');
+
 class UIComponent extends React.Component {
   render() {
     return(
       <div>
-        <h1>Hi Vinocur</h1>
-        <h1>Look at me</h1>
+        <h1>Look Up a Movie</h1>
+        <SearchComponent></SearchComponent>
       </div>
     );
   }

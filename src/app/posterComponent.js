b3b82c3a8a12f58e8bfb46680b75b8e1b8b048eb
Andrew <andrew.vinocur@gmail.com>
2017-09-26 17:46:38 -0700

    creates poster component

---
+++ src/app/posterComponent.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class PosterComponent extends React.Component {
+  render() {
+    return(
+      <img src="https://images-na.ssl-images-amazon.com/images/I/51Qvs9i5a%2BL._SL500_AC_SS350_.jpg"/>
+    );
+  }
+}
+
+module.exports = PosterComponent;

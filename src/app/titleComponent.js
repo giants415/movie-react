f58842b84c8b4ca0e76999798f9bdcfd643fd8d8
Andrew <andrew.vinocur@gmail.com>
2017-09-26 17:46:24 -0700

    creates title component

---
+++ src/app/titleComponent.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class TitleComponent extends React.Component {
+  render() {
+    return(
+      <h2>Cool Movie Part II: The Second</h2>
+    );
+  }
+}
+
+module.exports = TitleComponent;

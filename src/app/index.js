6ee3fb619bc881027ac8866cc06937e626068a25
Andrew <andrew.vinocur@gmail.com>
2017-09-29 14:33:08 -0700

    uses props for data passing

--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,12 @@ require('./style/final.css');
 var SearchComponent = require('./SearchComponent');
 var MovieComponent = require('./MovieComponent');
 
+let movies = {
+  poster: 'https://images-na.ssl-images-amazon.com/images/I/51Qvs9i5a%2BL._SL500_AC_SS350_.jpg',
+  title: 'Lord Potter and the Monsters',
+  summary: 'Destroys a ring or something'
+}
+
 class UIComponent extends React.Component {
 
   render() {
@@ -12,10 +18,10 @@ class UIComponent extends React.Component {
       <div className={"ui"}>
         <h1>Look Up a Movie</h1>
         <SearchComponent className={"search"} />
-        <MovieComponent className={"movie"} />
+        <MovieComponent className={"movie"} movies={this.props.movies} />
       </div>
     );
   }
 }
 
-ReactDOM.render(<UIComponent />, document.getElementById('movieWrapper'));
+ReactDOM.render(<UIComponent movies={movies}/>, document.getElementById('movieWrapper'));

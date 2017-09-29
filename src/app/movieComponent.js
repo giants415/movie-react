89e072192eafdf36269e28e88107a950105f4816
Andrew <andrew.vinocur@gmail.com>
2017-09-29 14:32:59 -0700

    refactors with future functionality to use props

--- src/app/movieComponent.js
+++ src/app/movieComponent.js
@@ -1,23 +1,14 @@
 var React = require('react');
 
+
 class MovieComponent extends React.Component {
-
-  constructor(props){
-    super(props);
-
-    this.movie = {
-      poster: 'https://images-na.ssl-images-amazon.com/images/I/51Qvs9i5a%2BL._SL500_AC_SS350_.jpg',
-      title: 'Lord Potter and the Monsters',
-      summary: 'Destroys a ring or something'
-    }
-  }
 
   render() {
     return(
       <div>
-        <img src={this.movie.poster} />
-        <p>Title: {this.movie.title}</p>
-        <p>Summary: {this.movie.summary}</p>
+        <img src={this.props.movies.poster} />
+        <p>Title: {this.props.movies.title}</p>
+        <p>Summary: {this.props.movies.summary}</p>
       </div>
     );
   }

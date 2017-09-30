542b67c12445f93e0df2afcf562461b2e138dd58
Andrew <andrew.vinocur@gmail.com>
2017-09-30 00:00:04 -0700

    builds access to search bar value

--- src/app/searchComponent.js
+++ src/app/searchComponent.js
@@ -1,10 +1,23 @@
 var React = require('react');
 
 class SearchComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: 'Enter a Movie Title'
+    };
+  }
+
+  updateSearch(event) {
+    this.setState({search: event.target.value});
+  }
+
+
+
   render() {
     return(
       <div>
-        <input type="text" /><button>Search</button>
+        <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)} /><button>Search</button>
       </div>
     );
   }

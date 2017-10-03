9e362f36085a3b08343af917f672f7206328ca21
Andrew <andrew.vinocur@gmail.com>
2017-10-03 09:27:42 -0700

    creates data ahead of search filter

--- src/app/index.js
+++ src/app/index.js
@@ -5,11 +5,28 @@ require('./style/final.css');
 var SearchComponent = require('./SearchComponent');
 var MovieComponent = require('./MovieComponent');
 
-let movies = {
-  poster: 'https://images-na.ssl-images-amazon.com/images/I/51Qvs9i5a%2BL._SL500_AC_SS350_.jpg',
-  title: 'Lord Potter and the Monsters',
-  summary: 'Destroys a ring or something'
-}
+let movies = [
+  {
+    poster: 'https://images-na.ssl-images-amazon.com/images/I/51Qvs9i5a%2BL._SL500_AC_SS350_.jpg',
+    title: 'Lord Potter and the Monsters',
+    summary: 'Destroys a ring or something'
+  },
+  {
+    poster: 'https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg',
+    title: 'Pacific Rim Monsters',
+    summary: 'Monsters are here and they must be destroyed'
+  },
+  {
+    poster: 'https://upload.wikimedia.org/wikipedia/en/6/65/Clerks_movie_poster%3B_Just_because_they_serve_you_---_.jpg',
+    title: 'Clerks 1',
+    summary: 'guys sell stuff from a store'
+  },
+  {
+    poster: 'https://upload.wikimedia.org/wikipedia/en/8/8e/Baby_Driver_poster.jpg',
+    title: 'Baby Driver',
+    summary: 'Drives fast like a car, nascar, nascar'
+  }
+]
 
 class UIComponent extends React.Component {
 
@@ -22,7 +39,7 @@ class UIComponent extends React.Component {
       </div>
     );
   }
-  
+
 }
 
 ReactDOM.render(<UIComponent movies={movies}/>, document.getElementById('movieWrapper'));
